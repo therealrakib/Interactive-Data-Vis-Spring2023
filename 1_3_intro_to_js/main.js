b1ff4afc0163ecaf7b7c801dbf350c065cf67d5f
Muhammad Rakibul Islam (Rakib) <imrakibul@gmail.com>
2023-02-14 16:56:15 -0500

    Update main.js

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+const counter = document.getElementById('counter');
+      const button = document.getElementById('clickButton');
+      let count = 0;
+
+      button.onclick = function() {
+        count++;
+        counter.innerText = count;
+      };
